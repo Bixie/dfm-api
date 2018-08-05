d6a072d1c2e7f424dcd780c26cc9d5ea1edd3c03
Matthijs Alles <admin@bixie.nl>
2018-08-05 14:33:16 +0200

    cleanup

--- src/Helpers/ApiKeyHelper.php
+++ src/Helpers/ApiKeyHelper.php
@@ -10,6 +10,10 @@ class ApiKeyHelper extends Helper {
 
     const HEADER_KEY_APITOKEN = 'HTTP_X_DFM_APITOKEN';
 
+    /**
+     * Test if a valid API key was provided
+     * @return bool
+     */
     public function test(){
         $api_key = isset($_SERVER[self::HEADER_KEY_APITOKEN]) ? $_SERVER[self::HEADER_KEY_APITOKEN] : '';
         return $api_key === $this->app['dfm.api_key'];
