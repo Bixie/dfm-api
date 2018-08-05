dc6325dd63afecc22e9d2514b75517624e7b36ed
Matthijs Alles <admin@bixie.nl>
2018-08-05 02:14:53 +0200

    rem config

--- config.php
+++ config.php
@@ -1,10 +1,13 @@
 <?php
 return [
-    'dfm_api_url' => '',
-    'dfm_api_key' => '',
+    'dfm_api' => [
+        'dfm.api_url' => 'http://localhost:3000',
+        'dfm.api_key' => 'gh*sgSHDsdg#$^34W^sVS(#$SG*$4g9gGR&3',
+    ],
     'lime' => [
         'debug' => true,
         'app.name' => 'DFM API Server',
         'sec-key' => 'lkfz48$w%8FiW44w(^#FKsdg#UWFoaf8F(W$',
+        'temppath.previewimages' => '..\image-web',
     ],
 ];
